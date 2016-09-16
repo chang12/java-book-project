92af33545a1270c20f4d990a09f0a62fd3214ae3
Changhyun Lee <chang12@snu.ac.kr>
2016-09-16 20:31:48 +0900

    전화번호 관리 프로그램 02단계
    - 반복문을 돌면서 원하는 메뉴를 Scanner로 입력받음.
    - Scanner로 PhoneInfo 인스턴스의 값을 입력받음.

--- src/kr/fakenerd/PhoneInfo.java
+++ src/kr/fakenerd/PhoneInfo.java
@@ -1,9 +1,13 @@
 package kr.fakenerd;
 
+import java.util.Scanner;
+
 public class PhoneInfo {
     private String name;
     private String phoneNumber;
     private String birth;
+
+    private static Scanner keyboard = new Scanner(System.in);
 
     public PhoneInfo(String name, String phoneNumber) {
         this.name = name;
@@ -24,10 +28,40 @@ public class PhoneInfo {
         System.out.println("");
     }
 
+    private static int showMenu() {
+        System.out.println("선택하세요...");
+        System.out.println("1. 데이터 입력");
+        System.out.println("2. 프로그램 종료");
+        System.out.print("선택: ");
+        int menu = keyboard.nextInt();
+        keyboard.nextLine(); // 1 혹은 2를 입력한 뒤 누른 Enter 키는 nextInt로 흡수되지 않는다. 이를 버리는 역할.
+        return menu;
+    }
+
+    private static PhoneInfo getPhoneInfo() {
+        System.out.print("이름 : ");
+        String name = keyboard.nextLine();
+        System.out.print("전화번호 : ");
+        String phoneNumber = keyboard.nextLine();
+        System.out.print("생년월일 : ");
+        String birth = keyboard.nextLine();
+        System.out.println("");
+        return new PhoneInfo(name, phoneNumber, birth);
+    }
+
     public static void main(String[] args) {
-        PhoneInfo pInfo1 = new PhoneInfo("김철수", "111-2222", "92.12.02");
-        PhoneInfo pInfo2 = new PhoneInfo("박영희", "333-4444", "92.12.02");
-        pInfo1.showPhoneInfo();
-        pInfo2.showPhoneInfo();
+        while (true) {
+            int menu = showMenu();
+            switch (menu) {
+                case 1:
+                    PhoneInfo pInfo = getPhoneInfo();
+                    System.out.println("입력된 정보 출력...");
+                    pInfo.showPhoneInfo();
+                    break;
+                case 2:
+                    System.exit(0);
+                default :
+            }
+        }
     }
 }
